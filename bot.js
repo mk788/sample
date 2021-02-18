59c28a23979afb19b1b0882d95a6d132e5914cbc
mk788 <mk788@kent.ac.uk>
2021-02-18 20:11:28 +0000

    "another useless commit"

--- bot.js
+++ bot.js
@@ -2,11 +2,12 @@
 function saySomething() {
   var message = document.getElementById('message').value.toLowerCase();
   var conversation = document.getElementById('conversation');
-  var answer = "Sorry Dave, I cannot do that."
-
-  if (message.indexOf("rain") != -1 || message.indexOf("sun") != -1 || message.indexOf("weather") != -1)
-    answer = "I do not care too much about weather, I'm locked inside a data center.";
-
+  var answer = answerQuery(message);
   conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
   conversation.innerHTML += "<p><strong>Useless bot:</strong> " + answer + "</p>";
+}
+function answerQuery(message) {
+  if (message.indexOf("rain") != -1 || message.indexOf("sun") != -1 || message.indexOf("weather") != -1)
+    return "I do not care too much about weather, I am just a Bot.";
+  return "Sorry Dave, I cannot do that.";
 }
